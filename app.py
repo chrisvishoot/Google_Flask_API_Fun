d72541a2fe5c36d608825a46c1f5914e7f288f66
chrisvishoot <ltdchaos@hotmail.com>
2016-11-12 12:48:45 -0800

    Got the latitude and longitude, and now trying to get the corresponding address. A few ideas: the google maps API has a reverse look up that returns the address from the lat and long based off of your IP address. Then, I can reroute to get directions to the location.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import urllib2
 import json
 import geocoder
@@ -7,19 +7,25 @@ LOCATION = '37.787930,-122.4074990'
 RADIUS = 5000
 url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
      '&radius=%s&sensor=false&key=%s') % (LOCATION, RADIUS, AUTH_KEY)
-ip_address = str(socket.gethostbyname(socket.gethostname()))
-geolocation_url = 'https://maps.googleapis.com/maps/api/geocode/json?&latlng=37.787930,-122.4074990'
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Welcome to our page!"
+    string_result = 'Welcome to our page!<br/>'
+    string_result += 'Web Page locations include:<br/>'
+    string_result += 'yelpCallIpLocation'
+    return render_template('index.html')
+@app.route('/PenguinsAreCool')
+def PenguinsAreCool():
+    return "So Penguins Are Cool"
+
 @app.route('/yelpCallIpLocation')
 def call():
+    #This is to get the longitude and the latitutde
     g = geocoder.ip('me')
     LOCATION = '%s,%s' %(g.lat, g.lng)
-    TYPE = 'restaurant'
+    TYPE = 'food'
     url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
          '&radius=%s&sensor=false&opennow=true&type=%s&key=%s') % (LOCATION, RADIUS, TYPE, AUTH_KEY)
     response = urllib2.urlopen(url)
@@ -28,7 +34,9 @@ def call():
     string_result = ""
     if json_data['status'] == 'OK':
         for place in json_data['results']:
-            string_result += '%s: %s\n' % (place['name'], place['geometry']['location']['lat'])
+            string_result += '%s: %s<br/>' % (place['name'], place['geometry']['location']['lat'])
+            #print ('%s: %s<br/>') % (place['name'], place['geometry']['location']['lat']['lng'])
+            print str(place)
     return string_result
 if __name__ == "__main__":
     app.run()
--- yelp_practice.py
+++ yelp_practice.py
@@ -12,7 +12,7 @@
 # import urllib2
 # import json
 #
-# AUTH_KEY = 'AIzaSyAs6zKhY63VwUB7QdXvuf96A45gmuh5UPw'
+# YOUR_API_KEY = 'AIzaSyAs6zKhY63VwUB7QdXvuf96A45gmuh5UPw'
 # LOCATION = '37.787930,-122.4074990'
 # RADIUS = 5000
 # geolocation_url = 'https://maps.googleapis.com/maps/api/geocode/json?&latlng=37.787930,-122.4074990'
@@ -24,15 +24,37 @@
 #         print place[0]['geometry']['location']
 
 import geocoder
+import urllib2
+import json
 g = geocoder.ip('me')
 print g.latlng
 print g.city
-
-# url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
-#      '&radius=%s&sensor=false&key=%s') % (LOCATION, RADIUS, AUTH_KEY)
-# response = urllib2.urlopen(url)
-# json_raw = response.read()
-# json_data = json.loads(json_raw)
-# if json_data['status'] == 'OK':
-#     for place in json_data['results']:
-#         print '%s: %s\n' % (place['name'], place['geometry']['location']['lat'])
+LOCATION = '%s,%s' %(g.lat, g.lng)
+RADIUS = 200
+YOUR_API_KEY = 'AIzaSyAs6zKhY63VwUB7QdXvuf96A45gmuh5UPw'
+url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
+     '&radius=%s&sensor=false&key=%s') % (LOCATION, RADIUS, YOUR_API_KEY)
+response = urllib2.urlopen(url)
+json_raw = response.read()
+json_data = json.loads(json_raw)
+if json_data['status'] == 'OK':
+    print json_data['results']
+    for place in json_data['results']:
+        #print '%s: %s %s\n' % (place['name'], place['geometry']['location']['lat'], place['geometry']['location']['lng'])
+        latitutde = float(place['geometry']['location']['lat'])
+        longitude = float(place['geometry']['location']['lng'])
+        print '%s, %s' % (latitutde, longitude)
+        g = geocoder.google([latitutde, longitude], method='reverse')
+        get_address_url = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=%s,%s&key=YOUR_API_KEY' % (latitutde, longitude)
+        response = urllib2.urlopen(url)
+        json_raw = response.read()
+        json_data = json.loads(json_raw)
+        address = ""
+        #address = json_data['results'][0]
+        print address
+        # address = ""
+        # address += g.city + ", "
+        # address += g.state + ", "
+        # address += g.country
+        print address + " Postal Code: " + g.postal
+        #g = geocoder.google([longitude, -latitutde], method='reverse')
