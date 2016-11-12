1de8b296379488c69b588f371c724997df2d098c
chrisvishoot <ltdchaos@hotmail.com>
2016-11-11 23:44:21 -0800

    Location is now based off of IP

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
 from flask import Flask
+import urllib2
+import json
+import geocoder
+AUTH_KEY = 'AIzaSyAs6zKhY63VwUB7QdXvuf96A45gmuh5UPw'
+LOCATION = '37.787930,-122.4074990'
+RADIUS = 5000
+url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
+     '&radius=%s&sensor=false&key=%s') % (LOCATION, RADIUS, AUTH_KEY)
+ip_address = str(socket.gethostbyname(socket.gethostname()))
+geolocation_url = 'https://maps.googleapis.com/maps/api/geocode/json?&latlng=37.787930,-122.4074990'
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World"
-
-@app.route('/yelpCall')
+    return "Welcome to our page!"
+@app.route('/yelpCallIpLocation')
 def call():
-    return "Derp"
+    g = geocoder.ip('me')
+    LOCATION = '%s,%s' %(g.lat, g.lng)
+    TYPE = 'restaurant'
+    url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
+         '&radius=%s&sensor=false&opennow=true&type=%s&key=%s') % (LOCATION, RADIUS, TYPE, AUTH_KEY)
+    response = urllib2.urlopen(url)
+    json_raw = response.read()
+    json_data = json.loads(json_raw)
+    string_result = ""
+    if json_data['status'] == 'OK':
+        for place in json_data['results']:
+            string_result += '%s: %s\n' % (place['name'], place['geometry']['location']['lat'])
+    return string_result
 if __name__ == "__main__":
     app.run()
--- yelp_practice.py
+++ yelp_practice.py
@@ -9,18 +9,30 @@
 # #     print place.name
 # #     print place.geo_location
 # #     print place.place_id
-import urllib2
-import json
+# import urllib2
+# import json
+#
+# AUTH_KEY = 'AIzaSyAs6zKhY63VwUB7QdXvuf96A45gmuh5UPw'
+# LOCATION = '37.787930,-122.4074990'
+# RADIUS = 5000
+# geolocation_url = 'https://maps.googleapis.com/maps/api/geocode/json?&latlng=37.787930,-122.4074990'
+# response = urllib2.urlopen(geolocation_url)
+# json_raw = response.read()
+# json_data = json.loads(json_raw)
+# if json_data['status'] == 'OK':
+#     for place in json_data['results']:
+#         print place[0]['geometry']['location']
 
-AUTH_KEY = 'AIzaSyAs6zKhY63VwUB7QdXvuf96A45gmuh5UPw'
-LOCATION = '37.787930,-122.4074990'
-RADIUS = 5000
+import geocoder
+g = geocoder.ip('me')
+print g.latlng
+print g.city
 
-url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
-     '&radius=%s&sensor=false&key=%s') % (LOCATION, RADIUS, AUTH_KEY)
-response = urllib2.urlopen(url)
-json_raw = response.read()
-json_data = json.loads(json_raw)
-if json_data['status'] == 'OK':
-    for place in json_data['results']:
-        print '%s: %s\n' % (place['name'], place['geometry']['location']['lat'])
+# url = ('https://maps.googleapis.com/maps/api/place/search/json?location=%s'
+#      '&radius=%s&sensor=false&key=%s') % (LOCATION, RADIUS, AUTH_KEY)
+# response = urllib2.urlopen(url)
+# json_raw = response.read()
+# json_data = json.loads(json_raw)
+# if json_data['status'] == 'OK':
+#     for place in json_data['results']:
+#         print '%s: %s\n' % (place['name'], place['geometry']['location']['lat'])
